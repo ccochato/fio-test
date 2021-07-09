0b3a68b48b0e3d72504d81e09fe82042e6b1a0c2
Carlos Cocha <ccocha@tave102-eth4.cscs.ch>
2021-07-09 05:33:18 +0200

    Script using slurm and appear a problem with the folder sys/class/net/ib0/statistics/

---
+++ test4.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --nodes=1
+
+#SBATCH --cpus-per-task=1
+#SBATCH --wait-all-nodes=1
+#SBATCH --job-name=cmssw_m2n_iobench
+#SBATCH --time=01:00
+
+rx1=$(cat /sys/class/net/eth0/statistics/rx_bytes)
+while sleep 1; do
+    rx2=$(cat /sys/class/net/eth0/statistics/rx_bytes)
+    timestamp=$(date +%s)
+    date=( $timestamp $rx2 )
+    printf 'Download rate: %s B/s\n' "$((rx2))"
+    printf "%s\n" "${date[*]}" >>data.txt    
+done
