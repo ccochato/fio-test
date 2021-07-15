c25d8e73dab743505090ae2aa02f252a3f7da967
Carlos Cocha <ccocha@tave102-eth4.cscs.ch>
2021-07-15 13:29:40 +0200

    Sequencial rad and bytes reading

---
+++ task_logs/servers_bytes2.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# cli
+LOGSDIR_JOB=$1
+NUM_CORES=$2
+DATADIR=$3
+
+THISHOST="`hostname`"
+INSTANCE=$SLURM_LOCALID
+
+
+# various variables
+WORKDIR="/var/tmp/fiorun__$INSTANCE"
+LOGSDIR_TASK="$LOGSDIR_JOB/${THISHOST}__$INSTANCE"
+
+# create the logs directory 
+[ -d "$LOGSDIR_TASK" ] && echo "$LOGSDIR_TASK already exists!!!" && exit 1
+mkdir $LOGSDIR_TASK
+
+[ -d "$WORKDIR" ] && rm -rf $WORKDIR
+mkdir $WORKDIR
+cd $WORKDIR
+
+
+# run cmsRun for instance 0
+if [ $INSTANCE -eq 0 ]
+then
+    fio /users/ccocha/FIO/fio-tests/task_logs/seqread.fio &
+
+    # loop and collect network usage 
+    # break out when the aboe process is finished
+    PID=$!
+    while true;
+    do
+        [ -n "${PID}" -a -d "/proc/${PID}" ] || break
+        timestamp=$(date +%s )
+        #bytes=`cat /sys/class/net/ib0/statistics/rx_bytes`
+        bytes=( $timestamp $(ifconfig | grep "UP\|RX packets"  | awk '{print $5}') )
+        printf " ${bytes[*]} " >> netlogs
+        printf " ${bytes[*]} " 
+        printf " \n " >>netlogs
+        unset bytes
+        sleep 1
+    done
+
+    # save the net logs
+    mv netlogs $LOGSDIR_TASK/
+else
+    # for all the other instances
+    fio /users/ccocha/FIO/fio-tests/task_logs/seqread.fio
+
+fi
+
+# move the logs if exist
+mv iotest $LOGSDIR_TASK/
---
+++ task_logs/test.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+#SBATCH --partition=normal
+#SBATCH --nodes=10
+#SBATCH --ntasks-per-node=6
+#SBATCH --cpus-per-task=1
+#SBATCH --wait-all-nodes=1
+#SBATCH --job-name=cmssw_m2n_iobench
+#SBATCH --time=02:00
+#SBATCH --output=/users/ccocha/FIO/fio-tests/task_logs/%j.log
+#SBATCH --error=/users/ccocha/FIO/fio-tests/task_logs/%j.err
+
+
+nodes=( 1 2 4 8 16 32 64)
+
+
+#chmod +x servers_bytes.sh
+
+#for i in ${nodes[*]}; do
+#  srun \
+#    -N1 \
+#    --mem=124G \
+#    
+#    bash servers_bytes.sh $i --subset $i --file $1 &
+#done
+
+#wait
+
+# general settings
+
+LOGSDIR_TOP=/users/ccocha/FIO/fio-tests/task_logs/job_logs
+NUM_CORES=$SLURM_CPUS_PER_TASK
+
+# based on the above
+LOGSDIR_JOB="$LOGSDIR_TOP/job_$SLURM_JOB_ID"
+
+# create a common dir for this job for the logs/results
+[ -d "$LOGSDIR_JOB" ] && rm -rf "$LOGSDIR_JOB"
+mkdir $LOGSDIR_JOB
+
+
+# run the stuff on each node for each task/exe
+srun servers_bytes2.sh $LOGSDIR_JOB $NUM_CORES $DATADIR
+
+
