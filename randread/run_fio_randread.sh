2d77031afe8d438db53bef90222d1acfd9fc60d9
Carlos Cocha <ccocha@tave101-eth4.cscs.ch>
2021-07-19 23:29:53 +0200

    random read implementation

---
+++ randread/run_fio_randread.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#SBATCH --partition=normal
+#SBATCH --nodes=128
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --wait-all-nodes=1
+#SBATCH --job-name=cmssw_m2n_iobench
+#SBATCH --time=60:00
+#SBATCH --output=/users/ccocha/FIO/fio-tests/randread/128_node/%j.log
+#SBATCH --error=/users/ccocha/FIO/fio-tests/randread/128_node/%j.err
+
+
+# general settings
+fio_njob=64
+fio_directory=/scratch/snx2000/ccocha/randread/128_node
+LOGSDIR_TOP=/users/ccocha/FIO/fio-tests/randread/128_node
+
+# based on the above
+LOGSDIR_JOB="$LOGSDIR_TOP/job_${SLURM_JOB_ID}_fiojob_$fio_njob"
+
+# create a common dir for this job for the logs/results
+[ -d "$LOGSDIR_JOB" ] && rm -rf "$LOGSDIR_JOB"
+mkdir $LOGSDIR_JOB
+
+
+# run the stuff on each node for each fio_njob
+srun fio_randread.sh $LOGSDIR_JOB $fio_njob $fio_directory
+
+
+
