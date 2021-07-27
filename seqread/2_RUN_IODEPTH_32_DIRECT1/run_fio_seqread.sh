896903ce0d861161857159740ff042400528ee9f
Carlos Cocha <ccocha@tave101-eth4.cscs.ch>
2021-07-27 17:56:45 +0200

    new run

---
+++ seqread/2_RUN_IODEPTH_32_DIRECT1/run_fio_seqread.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#SBATCH --partition=normal
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=1
+#SBATCH --wait-all-nodes=1
+#SBATCH --job-name=fio_seqread
+#SBATCH --time=60:00
+
+# general settings
+fio_njob=$1
+LOGSDIR_TOP=$2
+fio_directory=$3
+
+# based on the above
+LOGSDIR_JOB="$LOGSDIR_TOP/job_${SLURM_JOB_ID}_fiojob_$fio_njob"
+
+# create a common dir for this job for the logs/results
+[ -d "$LOGSDIR_JOB" ] && rm -rf "$LOGSDIR_JOB"
+mkdir $LOGSDIR_JOB
+
+
+# run the stuff on each node for each fio_njob
+srun fio_seqread.sh $LOGSDIR_JOB $fio_njob $fio_directory
+
+
+
